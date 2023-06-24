c04b562fba27b73724e8233226bee93319f76cb9
Netology test <n89500554589@gmail.com>
2023-06-24 22:30:31 +0800

    commit23

--- src/test/kotlin/CommissionTest.kt
+++ src/test/kotlin/CommissionTest.kt
@@ -11,7 +11,7 @@ class CommissionTest {
 
         val result = amountFee(amount = amount, amountMonth = amountMonth, tCards = tCard)
 
-        assertEquals(-2, result)
+        assertEquals(-1, result)
     }
 
     @Test
